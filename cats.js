5ce9a8916f156f5b61b23613fffcb276b2258cbf
Lia Forgione <liaforg@gmail.com>
2017-07-11 12:56:02 -0400

    setting up

---
+++ cats.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+  var summonKitties = document.getElementsByClassName('summon-cats');
+
+  runSummonKitties = document.addEventListener('click', function(event){
+    console.log('click');
+
+  })
+
+
+
+});
