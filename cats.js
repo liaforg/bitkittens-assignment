00d37cd0ceddf180584b24df0092bd5b0be55267
Lia Forgione <liaforg@gmail.com>
2017-07-11 15:24:04 -0400

    completed

--- cats.js
+++ cats.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+  var kittenCount = 1
   var summonKitties = document.getElementsByClassName('summon-cats');
 
   runSummonKitties = document.addEventListener('click', function(event){
@@ -10,9 +11,15 @@ document.addEventListener("DOMContentLoaded", function() {
       method: 'GET',
       dataType: 'json'
     })
+    .done(function (responseData) {
+        var img = document.createElement('img');
+        var kittenPicSrc = responseData.cats[0].photo;
+        img.src = kittenPicSrc
 
-  })
+        document.querySelector('#cat'+kittenCount).append(img);
+        kittenCount = kittenCount + 1;
 
-
+    });
+  });
 
 });
